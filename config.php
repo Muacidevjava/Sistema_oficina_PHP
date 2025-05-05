0ce39c4e2dcd6dc7b1f605667c0d4e547c51291f
MuaciDevjava <muaci@yahoo.com>
2025-05-05 20:21:03 -0300

    arquivo de configuração

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+$nome_oficina = "Oficina Freitas";
+$url = "http://localhost/oficina/";
+$endereco_loja = "Rua noventa e dois, 123, Caetés I, abreu e lima,pe";
+$telefone_loja = "(81) 99999-9999";
+$email_adm = "contato@oficina.com";
+
+
+
+?>
