8bf3b180cda78632d9844a21cbba76fe0f2d3b5c
MuaciDevjava <muaci@yahoo.com>
2025-05-06 11:13:07 -0300

    Metodo para Iserir dados no Banco

--- painel-adm/mecanicos/inserir.php
+++ painel-adm/mecanicos/inserir.php
@@ -6,8 +6,26 @@ $cpf = $_POST['cpf_mec'];
 $email = $_POST['email_mec'];
 $endereco = $_POST['endereco_mec'];
 
+$antigo = $_POST['antigo'];
+$id = $_POST['txtid2'];
+
+
+//verificar se o Registro já existe no banco de dados
 $query = $pdo->query("SELECT * FROM mecanicos where cpf ='$cpf'");
 $res = $query->fetchAll(PDO::FETCH_ASSOC);
 $tota_reg = @count($res);
-echo $tota_reg;
+if($tota_reg > 0){
+	echo 'CPF já cadastrado!';
+	exit();
+}
+//Metodo para inserir dados no banco de dados
+$res = $pdo->prepare("INSERT INTO mecanicos (nome, telefone, cpf, email, endereco) VALUES (:nome, :telefone, :cpf, :email, :endereco)");
+
+$res->bindValue(":nome", $nome);
+$res->bindValue(":telefone", $telefone);
+$res->bindValue(":cpf", $cpf);
+$res->bindValue(":email", $email);
+$res->bindValue(":endereco", $endereco);
+$res->execute();
+echo "salvo com sucesso";
 ?>
