acef8a997b44820cfe9ee6844a3fc98f1a19b0e6
MuaciDevjava <muaci@yahoo.com>
2025-05-06 19:08:32 -0300

    Redirecionamento

--- autenticar.php
+++ autenticar.php
@@ -14,12 +14,22 @@ $total_reg = $query->rowCount();
 if($total_reg > 0){
     $res = $query->fetchAll(PDO::FETCH_ASSOC);
     session_start();
-    $_SESSION['id_usuario'] = $res[0]['id'];
-    $_SESSION['nome_usuario'] = $res[0]['nome'];
-    $_SESSION['nivel_usuario'] = $res[0]['nivel'];
+    $_SESSION['id'] = $res[0]['id'];
+    $_SESSION['nome'] = $res[0]['nome'];
+    $_SESSION['nivel'] = $res[0]['nivel'];
     
-    echo 'Bem vindo!';
+    // Redirecionamento baseado no nível do usuário
+    if($_SESSION['nivel'] == 'admin'){
+        echo "<script>window.location='painel-adm'</script>";
+    }
+    else if($_SESSION['nivel'] == 'mecanico'){
+        echo "<script>window.location='painel-mecanico'</script>";
+    }
+    else if($_SESSION['nivel'] == 'recepcionista'){
+        echo "<script>window.location='painel-recep'</script>";
+    }
 } else {
-    echo 'Dados Incorretos!';
+    echo "<script>window.alert('Dados Incorretos!')</script>";
+    echo "<script>window.location='index.php'</script>";
 }
 ?>
