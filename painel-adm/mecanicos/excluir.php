7a03d15e0c0f4c9bc4a56c70431a43d33acab279
MuaciDevjava <muaci@yahoo.com>
2025-05-07 09:56:55 -0300

    excluido em duas tabaelas

--- painel-adm/mecanicos/excluir.php
+++ painel-adm/mecanicos/excluir.php
@@ -2,6 +2,23 @@
 require_once("../../conexao.php");
 
 $id = $_POST['id'];
-$res = $pdo->query("DELETE FROM mecanicos WHERE id = '$id'");
+
+// Buscar o CPF do mecânico antes de excluir
+$query = $pdo->prepare("SELECT cpf FROM mecanicos WHERE id = :id");
+$query->bindValue(":id", $id);
+$query->execute();
+$res = $query->fetchAll(PDO::FETCH_ASSOC);
+$cpf = $res[0]['cpf'];
+
+// Excluir da tabela mecanicos
+$res = $pdo->prepare("DELETE FROM mecanicos WHERE id = :id");
+$res->bindValue(":id", $id);
+$res->execute();
+
+// Excluir também da tabela usuarios
+$res = $pdo->prepare("DELETE FROM usuarios WHERE cpf = :cpf");
+$res->bindValue(":cpf", $cpf);
+$res->execute();
+
 echo "Excluído com Sucesso!";
 ?>
